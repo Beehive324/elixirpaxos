56a4e650fd54e04a27ec6b31777844081bd4d709
Fairson <fs00602@surrey.ac.uk>
2024-11-18 18:35:51 +0000

    Initial commit

--- lib/elixirpaxos.ex
+++ lib/elixirpaxos.ex
@@ -1,7 +1,40 @@
 defmodule Elixirpaxos do
+
+  def start(name, participants) do
+    pid = spawn(Elixirpaxos, :init, [name, paxos_proc])
+    pid = case :global.re_register_name(name, pid) do
+      :yes -> pid
+      :no -> nil
+    end
+    IO.puts(if pid, do: "registered #{name}", else: "failed to register #{name}")
+  end
   @moduledoc """
   Documentation for `Elixirpaxos`.
   """
+
+
+  def init(name, participants) do
+
+    state = %{
+    name: name,
+
+    }
+  end
+
+
+  def propose(pid, inst, value, t) do
+    {:decision, v}
+
+    {:abort}
+
+    {:timeout}
+  end
+
+
+
+  defp get_decision(pid, inst, t) do
+
+  end
 
   @doc """
   Hello world.
