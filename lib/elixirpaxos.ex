74c48f3ff8485452fff88eb698e745cf75187a05
Fairson <fs00602@surrey.ac.uk>
2024-11-18 18:35:51 +0000

    Initial commit

---
+++ lib/elixirpaxos.ex
@@ -0,0 +1,18 @@
+defmodule Elixirpaxos do
+  @moduledoc """
+  Documentation for `Elixirpaxos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixirpaxos.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixirpaxos_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirpaxosTest do
+  use ExUnit.Case
+  doctest Elixirpaxos
+
+  test "greets the world" do
+    assert Elixirpaxos.hello() == :world
+  end
+end
