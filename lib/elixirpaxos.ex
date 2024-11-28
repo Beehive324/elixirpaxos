a925709e73b037bfb6f2932ea030e1038d6986d5
Fairson <fs00602@surrey.ac.uk>
2024-11-28 15:48:42 +0000

    file changes

--- lib/elixirpaxos.ex
+++ lib/elixirpaxos.ex
@@ -1,7 +1,6 @@
-defmodule Elixirpaxos do
-
+defmodule Paxos do
   def start(name, participants) do
-    pid = spawn(Elixirpaxos, :init, [name, paxos_proc])
+    pid = spawn(Paxos, :init, [name, paxos_proc])
     pid = case :global.re_register_name(name, pid) do
       :yes -> pid
       :no -> nil
@@ -9,49 +8,43 @@ defmodule Elixirpaxos do
     IO.puts(if pid, do: "registered #{name}", else: "failed to register #{name}")
   end
   @moduledoc """
-  Documentation for `Elixirpaxos`.
+  Documentation for `Paxos`.
+    need the eventual leader
+
   """
 
-
   def init(name, participants) do
-
     state = %{
     name: name,
     participants: participants,
+    leader: leader,
+    acceptors: [],
+    value: 0,
 
     }
+    state
+    run(state)
   end
 
 
   def run(state) do
 
+
+
+
   end
 
-
-  def propose(pid, inst, value, t) do
+  defp propose(pid, inst, value, t) do
     {:decision, v}
 
     {:abort}
-
     {:timeout}
   end
 
-
-
   defp get_decision(pid, inst, t) do
 
+
   end
 
-  @doc """
-  Hello world.
 
-  ## Examples
-
-      iex> Elixirpaxos.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
