932f0ab89222fe436009094619cf950e2aa3b2bf
Fairson <fs00602@surrey.ac.uk>
2025-01-21 12:09:37 +0000

    messages

--- lib/paxos.ex
+++ lib/paxos.ex
@@ -1,249 +1,178 @@
 defmodule Paxos do
 
-  #spwan each processc
-  def start(name, paxos_proc) do
-  # Unregister if the process name is already taken
-  if Process.whereis(name) do
-    IO.puts("Process #{inspect(name)} already exists. Unregistering...")
-    Process.unregister(name)
-  end
 
-  # Spawn and register the process
-  pid = spawn(Paxos, :init, [name, paxos_proc])
-
-  case :global.re_register_name(name, pid) do
-    :yes ->
-      IO.puts("Registered #{name} globally.")
-      pid
-
-    :no ->
-      IO.puts("Failed to register #{name} globally. Name already in use.")
-      nil
-  end
-end
-
-
-
-  @moduledoc """
-  Paxos Implementation for Solving Consensus
-  Every process acts as a acceptor, learner and proposer
-  """
-
-  #Implementing BestEffortBroadCast abstraction
-  def get_beb_name() do
-    {:registered_name, parent} = Process.info(self(), :registered_name)
-    String.to_atom(Atom.to_string(parent) <> "_beb")
-  end
-
-  def start_beb(name, participants) do
-    beb_name = String.to_atom("#{name}_beb")
-    if Process.whereis(beb_name) do
-      IO.puts("Process #{inspect(beb_name)} already registered. Unregistering...")
-      Process.unregister(beb_name)
-    end
-    Process.register(self(), name)
-    pid = spawn(BestEffortBroadcast, :init, [name, participants])
-    Process.register(pid, beb_name)
-    Process.link(pid)
-  end
-
-  def beb_broadcast(m, dest) do
-    BestEffortBroadcast.beb_broadcast(Process.whereis(get_beb_name()), m, dest)
-  end
-
-  #Implementing LeaderELection abstraction
-  defp get_le_name() do
-    {:registered_name, parent} = Process.info(self(), :registered_name)
-    String.to_atom(Atom.to_string(parent) <> "_eld")
-  end
-
-  defp start_le(name, processes) do
-    eld_name = String.to_atom("#{name}_eld")
-
-    if Process.whereis(eld_name) do
-      IO.puts("Process #{inspect(eld_name)} already registered")
-      Process.unregistered(eld_name)
+  def start(name, participants) do
+    # Unregister if the process name is already taken
+    if Process.whereis(name) do
+      Process.unregister(name)
     end
 
-    pid = spawn(EventualLeaderDetector, :init, [name, processes])
+    # Spawn and register the Paxos process
+    pid = spawn(Paxos, :init, [name, participants])
 
-    Process.register(pid, eld_name)
-    Process.link(pid)
-
+    case :global.re_register_name(name, pid) do
+      :yes -> pid
+      :no -> nil
+    end
   end
 
-  def leader_detector(processes) do
-  case EventualLeaderDetector.determine_leader(processes) do
-    nil ->
-      IO.puts("No leader could be determined.")
-      nil
+  def propose(pid, inst, value, timeout) do
+    send(pid, {:propose, inst, value, self()})
 
-    leader ->
-      IO.puts("The current leader is: #{leader}")
-      leader
-  end
-end
-
-
-  #Two steps in paxos: propose, accept
-  #allows processes to propose values
-  def propose(pid, inst, value, t) do
-    send(pid, {:propose, inst, {:val, value}})
     receive do
-      {:decision, value} -> {:decision, value}
-      {:abort, value} -> {:abort}
+      {:decision, ^inst, decision_value} -> {:decision, decision_value}
+      {:abort, ^inst} -> {:abort}
     after
-    t -> {:timeout}
+      timeout -> {:timeout}
     end
   end
 
-  #get_decision api
-  def get_decision(pid, inst, t) do
-    send(pid, {:get_decision, self(),inst})
+  def get_decision(pid, inst, timeout) do
+    send(pid, {:get_decision, inst, self()})
+
     receive do
-      {:decision, val, value} -> value
+      {:decision, ^inst, value} -> value
     after
-      t -> nil
+      timeout -> nil
     end
   end
 
+  # ==================== Internal Implementation ====================
 
-  #  { ProcessID : ( proposal_num , proposal_val ) }
   def init(name, participants) do
-    #start beb brodcast
-    start_beb(name, participants)
-
-    #start leader detector
-    start_le(name, participants)
-    #split between proposer and acceptor, n - 2
     state = %{
-    name: name, # { prepare request: (1, proceess_id) }
-    inst: nil,
-    sender: nil,
-    participants: participants,
-    prep_requests: %MapSet{}, #store prepare requests
-    proposals: %MapSet{}, #proposal num, value
-    proposal_number: 1,
-    proposal_value: 0,
-    decisions: %MapSet{},
-    promises: nil, #have a map to store promises
-    quorum_size: nil,
-    bal: 0, #indicates which round of paxos, increment every time theres is a new round of paxos
-    a_bal: 0, #highest proposal number
-    a_val: 0, #accepted value
-    #start each process at {1, process_id}, counter and process id
-    v: nil
+      name: name,
+      participants: participants,
+      quorum_size: div(length(participants), 2) + 1,
+      proposals: %{}, # Tracks proposals by instance
+      decisions: %{}, # Tracks decided values by instance
+      promises: %{}, # Tracks promises received in Prepare phase
+      accepts: %{},  # Tracks accept messages in Accept phase
+      bal: 0,        # Current ballot number
+      a_bal: 0,      # Highest promised ballot number
+      a_val: nil     # Value associated with the highest promised ballot
     }
-    state
+
+    Process.register(self(), name)
     run(state)
   end
 
-  #handle promises
-  #list
-
-  #receive promises
-  def receivePromises(0, list) , do: list
-  def receivePromises(n, list) when n>0 do
+  defp run(state) do
     receive do
-      {:promiseOk, tStore, cmd, pid} ->
-        receivePromises(n-1, [{tStore, cmd, pid} |list])
-    after
-      # wait for 1s
-      1_000 ->
-        receivePromises(n-1, list)
+      {:propose, inst, value, sender} ->
+        state = handle_propose(state, inst, value, sender)
+        run(state)
+
+      {:prepare, inst, bal, sender} ->
+        state = handle_prepare(state, inst, bal, sender)
+        run(state)
+
+      {:promise, inst, bal, a_bal, a_val, sender} ->
+        state = handle_promise(state, inst, bal, a_bal, a_val, sender)
+        run(state)
+
+      {:accept, inst, bal, value, sender} ->
+        state = handle_accept(state, inst, bal, value, sender)
+        run(state)
+
+      {:accepted, inst, bal, sender} ->
+        state = handle_accepted(state, inst, bal, sender)
+        run(state)
+
+      {:commit, inst, value} ->
+        state = handle_commit(state, inst, value)
+        run(state)
+
+      {:get_decision, inst, sender} ->
+        decision = Map.get(state.decisions, inst, nil)
+        send(sender, {:decision, inst, decision})
+        run(state)
     end
   end
 
-  #receive proposals
-  def receiveProposals(0, acc) , do: acc
-  def receiveProposals(n, acc\\0) when n>0 do
-    receive do
-      :proposalSuccess ->
-        # proposerSay "Received 'ProposalSuccess'"
-        receiveProposals(n-1, acc+1)
-    after
-      # wait for 1s
-      1_000 ->
-        # proposerSay "Proposal didn't come"
-        receiveProposals(n-1, acc)
+  # ==================== Handle Events ====================
+
+  defp handle_propose(state, inst, value, sender) do
+    case state.decisions[inst] do
+      nil ->
+        # Start Prepare phase
+        new_bal = state.bal + 1
+        state = %{state | bal: new_bal}
+        broadcast(state.participants, {:prepare, inst, new_bal, state.name})
+        %{state | proposals: Map.put(state.proposals, inst, {value, sender}), promises: Map.put(state.promises, inst, [])}
+
+      decision ->
+        # Already decided
+        send(sender, {:decision, inst, decision})
+        state
     end
   end
 
-  #Leader Based functions
-  #(1) Broadcast prepare
-  def run(state) do
-  receive do
-    # Prepare Phase
-    {:prepare, value, t} ->
-      leader = EventualLeaderDetector.determine_leader(Process.whereis(String.to_atom("#{state.name}_eld")))
+  defp handle_prepare(state, inst, bal, sender) do
+    if bal > state.a_bal do
+      state = %{state | a_bal: bal}
+      send(sender, {:promise, inst, bal, state.a_bal, state.a_val, state.name})
+    else
+      send(sender, {:nack, inst, bal, state.name})
+    end
 
-      if state.name == leader do
-        prepare_req = {self(), state.proposal_number}
-        beb_broadcast({:prepare, prepare_req}, state.participants)
-
-        proposal = {state.proposal_number, state.proposal_value}
-        state = %{state |
-          prep_requests: MapSet.put(state.prep_requests, prepare_req),
-          proposal_number: state.proposal_number + 1,
-          bal: state.proposal_number + 1,
-          proposal_value: value,
-          quorom_size: div(length(state.participants), 2) + 1,
-          proposals: MapSet.put(state.proposals, proposal)
-        }
-
-        promises = receivePromises(state.quorom_size, [])
-        state = %{state | promises: promises}
-
-        if length(state.promises) >= div(state.quorom_size, 2) do
-          max_promise = Enum.max_by(state.promises, fn {a_bal, _a_val, _pid} -> a_bal end, fn -> {0, nil, nil} end)
-          {highest_a_bal, highest_a_val, _pid} = max_promise
-          new_value =
-            if highest_a_val == nil do
-              state.proposal_value
-            else
-              highest_a_val
-            end
-
-          state = %{state | proposal_value: new_value}
-          IO.puts("Quorum reached. Selected value: #{new_value}")
-        else
-          state = %{state | proposal_number: state.proposal_number + 1, bal: state.proposal_number + 1}
-          IO.puts("Quorum not reached. Retrying...")
-        end
-
-        state = %{state |
-          proposals: MapSet.put(state.proposals, {state.proposal_number, state.proposal_value})
-        }
-
-        beb_broadcast({:accept, {state.proposal_number, state.proposal_value}}, state.participants)
-      end
-
-    # Accept Phase
-    {:quorom, {:prepared, b, a_bal, a_val, v}} ->
-      if a_val == nil do
-        state = %{state | v: 0}
-      else
-        state = %{state | v: a_val}
-      end
-
-      beb_broadcast({:accept, state.b, state.v}, state.participants)
-
-    {:accept, {b, v}} ->
-      if b >= state.bal do
-        state = %{state | bal: b, a_bal: b, a_val: v}
-        beb_broadcast({:accepted, b}, state.participants)
-      else
-        send({:nack, b}, state.participants)
-      end
-
-    {:accept_quorom, pid, inst, val} ->
-      decision = get_decision(pid, inst, val)
-      state = %{state | a_val: decision}
-      IO.puts("Final decision value: #{state.v}")
+    state
   end
 
-  run(state) # Keep the process alive by recursively calling `run/1`.
-end
+  defp handle_promise(state, inst, bal, a_bal, a_val, sender) do
+    if bal == state.bal do
+      promises = Map.get(state.promises, inst, []) ++ [{a_bal, a_val, sender}]
+      state = %{state | promises: Map.put(state.promises, inst, promises)}
 
-end
+      if length(promises) >= state.quorum_size do
+        # Quorum reached, select a value to propose
+        highest_a_val = Enum.max_by(promises, fn {a_bal, _, _} -> a_bal end, fn -> {0, state.proposals[inst] |> elem(0)} end)
+        value_to_propose = elem(highest_a_val, 1)
+        broadcast(state.participants, {:accept, inst, bal, value_to_propose, state.name})
+        state = %{state | accepts: Map.put(state.accepts, inst, [])}
+      end
 
+      state
+    else
+      state
+    end
+  end
+
+  defp handle_accept(state, inst, bal, value, sender) do
+    if bal >= state.a_bal do
+      state = %{state | a_bal: bal, a_val: value}
+      send(sender, {:accepted, inst, bal, state.name})
+    else
+      send(sender, {:nack, inst, bal, state.name})
+    end
+
+    state
+  end
+
+  defp handle_accepted(state, inst, bal, sender) do
+    accepts = Map.get(state.accepts, inst, []) ++ [sender]
+    state = %{state | accepts: Map.put(state.accepts, inst, accepts)}
+
+    if length(accepts) >= state.quorum_size do
+      # Quorum reached in Accept phase, commit the value
+      {value, _} = state.proposals[inst]
+      broadcast(state.participants, {:commit, inst, value})
+      state = handle_commit(state, inst, value)
+    end
+
+    state
+  end
+
+  defp handle_commit(state, inst, value) do
+    %{state | decisions: Map.put(state.decisions, inst, value)}
+  end
+
+
+  defp broadcast(participants, message) do
+    Enum.each(participants, fn participant ->
+      if participant != self() do
+        send(:global.whereis_name(participant), message)
+      end
+    end)
+  end
+end
--- lib/storage_server.ex
+++ lib/storage_server.ex
@@ -1,60 +1,93 @@
 defmodule StorageServer do
-    def start(name, paxos_proc) do
-        pid = spawn(StorageServer, :init, [name, paxos_proc])
-        pid = case :global.re_register_name(name, pid) do
-            :yes -> pid
-            :no  -> nil
-        end
-        IO.puts(if pid, do: "registered #{name}", else: "failed to register #{name}")
-        pid
+  @moduledoc """
+  A storage server implementation that mimics a replicated data store using Paxos for achieving consensus.
+  This server ensures consistency across replicas by coordinating with Paxos processes.
+  """
+
+  def start(name, paxos_proc) do
+    pid = spawn(StorageServer, :init, [name, paxos_proc])
+    pid = case :global.re_register_name(name, pid) do
+      :yes -> pid
+      :no -> nil
     end
+    IO.puts(if pid, do: "registered #{name}", else: "failed to register #{name}")
+    pid
+  end
 
-    def init(name, paxos_proc) do
-        state = %{
-            name: name,
-            pax_pid: get_paxos_pid(paxos_proc),
-            last_instance: 0,
-            pending: {0, nil},
-            balance: 0,
+  def init(name, paxos_proc) do
+    state = %{
+      name: name,
+      pax_pid: get_paxos_pid(paxos_proc),
+      last_instance: 0,
+      pending: {0, nil},
+      data_store: %{} # Key-value store to mimic a replicated database
+    }
 
-        }
-        # Ensures shared destiny (if one of the processes dies, the other one does too)
-        # Process.link(state.pax_pid)
+    # Ensures shared destiny (if one of the processes dies, the other does too)
+    Process.link(state.pax_pid)
+
+    run(state)
+  end
+
+  # Retrieves the PID of the Paxos instance to connect to
+  defp get_paxos_pid(paxos_proc) do
+    case :global.whereis_name(paxos_proc) do
+      pid when is_pid(pid) -> pid
+      :undefined -> raise("Paxos process #{Atom.to_string(paxos_proc)} not found")
+    end
+  end
+
+  defp run(state) do
+    receive do
+      {:write, key, value, sender} ->
+        new_instance = state.last_instance + 1
+        state = %{state | last_instance: new_instance, pending: {new_instance, sender}}
+        send(state.pax_pid, {:propose, new_instance, {key, value}, self()})
+        run(state)
+
+      {:read, key, sender} ->
+        value = Map.get(state.data_store, key, :not_found)
+        send(sender, {:read_reply, key, value})
+        run(state)
+
+      {:poll_for_decisions} ->
+        state = poll_for_decisions(state)
+        run(state)
+
+      {:decision, instance, {key, value}} ->
+        if instance == state.last_instance do
+          # Update the data store and notify the sender if it matches the pending instance
+          {_instance, sender} = state.pending
+          new_data_store = Map.put(state.data_store, key, value)
+          send(sender, {:write_ack, key, value})
+          state = %{state | data_store: new_data_store, pending: {0, nil}}
+        end
         run(state)
     end
+  end
 
-    # Get pid of a Paxos instance to connect to
-    defp get_paxos_pid(paxos_proc) do
-        case :global.whereis_name(paxos_proc) do
-                pid when is_pid(pid) -> pid
-                :undefined -> raise(Atom.to_string(paxos_proc))
-        end
+  defp poll_for_decisions(state) do
+    send(state.pax_pid, {:get_decision, state.last_instance, self()})
+    receive do
+      {:decision, instance, {key, value}} when instance == state.last_instance ->
+        IO.puts("#{state.name}: Decided value #{inspect(value)} for key #{inspect(key)} in instance #{instance}")
+        new_data_store = Map.put(state.data_store, key, value)
+        %{state | data_store: new_data_store, pending: {0, nil}}
+      after
+        1000 ->
+          IO.puts("#{state.name}: No decision yet for instance #{state.last_instance}")
+          state
     end
+  end
 
-    defp wait_for_reply(_, 0), do: nil
-    defp wait_for_reply(r, attempt) do
-        msg = receive do
-            msg -> msg
-            after 1000 ->
-                send(r, {:poll_for_decisions})
-                nil
-        end
-        if msg, do: msg, else: wait_for_reply(r, attempt-1)
+  defp wait_for_reply(_, 0), do: nil
+  defp wait_for_reply(r, attempt) do
+    msg = receive do
+      msg -> msg
+      after 1000 ->
+        send(r, {:poll_for_decisions})
+        nil
     end
-
-
-
-
-
-
-
-    def run(state) do
-
-    end
-
-
-    defp poll_for_decisions(state) do
-
-    end
-
+    if msg, do: msg, else: wait_for_reply(r, attempt - 1)
+  end
 end
